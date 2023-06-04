63b8997c62411e1a0c3d87f344367af2556cd507
Sean Kelliher <sbkelliher@icloud.com>
2023-06-04 13:41:56 -0400

    Add prop.

--- src/components/HeaderUni.vue
+++ src/components/HeaderUni.vue
@@ -1,18 +1,14 @@
 <script setup>
-//import { ref } from "vue";
-
-/*defineProps({
-    title: {
+defineProps({
+    pageName: {
         type: String,
         required: true
     }
-});*/
-
-// const count = ref(0);
+});
 </script>
 
 <template>
     <header>
-        <h1>Airport Arrivals &amp; Departures</h1>
+        <h1>{{ pageName }}</h1>
     </header>
 </template>
